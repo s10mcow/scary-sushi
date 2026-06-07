a914026b388493b7613c2abe7d5cff29c9f79d8d
leif moocow <leifmoocow@leifs-MacBook-Pro.local>
2026-06-08 02:40:10 +0500

    Move Chapter 7 rocking chair

--- src/scene/createChapterSeven.ts
+++ src/scene/createChapterSeven.ts
@@ -1242,7 +1242,7 @@ export function createChapterSeven(): ChapterSevenData {
   );
   addBed(-23.55, 10.6, 1);
   addBed(-23.55, -10.6, -1);
-  addRockingChair(-20.25, -10.6, -Math.PI / 2);
+  addRockingChair(1200.10 - CENTER_X, 63.31 - HOUSE_CENTER_Z, -Math.PI / 2);
   addDiningTable(leftRoomCenterX, 0);
   addBookshelf(-25.05, -1.25, Math.PI / 2);
   const houseDrawer = addDrawer(-25.05, 2.4, Math.PI / 2, 'Table Drawer');
