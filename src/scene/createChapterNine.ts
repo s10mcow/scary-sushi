189c86a8748bf3ab7d6babf42ea07a3a64557665
leif moocow <leifmoocow@leifs-MacBook-Pro.local>
2026-06-21 21:53:20 +0500

    Add Chapter 9 small wall filler

--- src/scene/createChapterNine.ts
+++ src/scene/createChapterNine.ts
@@ -730,6 +730,7 @@ export function createChapterNine(): ChapterNineData {
   addAngledWall(4.33, 29.67, 4.39, 14.98);
   addAngledWall(-5.77, 14.95, -9.75, 6.94);
   addAngledWall(-6.16, 14.82, -9.09, 15.05);
+  addCurvedWall(-5.77, 14.95, -5.98, 14.99, -6.16, 14.82, 3);
   const shellColliders = colliders.slice();
 
   const shellObjects = [
